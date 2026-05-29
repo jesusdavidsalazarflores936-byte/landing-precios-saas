c333ffa623e73a97df31081c83a6d15edc61ef9b
Jesus David Salazar <jesusdavidsalazarflores936@gmail.com>
2026-05-29 02:46:58 -0500

    fix: remover jsdoc para evitar error de typescript

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  assetPrefix: isProd ? 'https://landing-precios-saas.vercel.app' : undefined,
+
   turbopack: {
     root: process.cwd(),
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
